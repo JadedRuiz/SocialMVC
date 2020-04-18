a9d1a926ac5e539ed5ae29348b289db9c44d5bdd
jefreyu12 <sebassdc@hotmail.com>
2020-04-18 17:08:49 -0500

    agregado, ver edad

--- SocialMVC/Models/User.cs
+++ SocialMVC/Models/User.cs
@@ -17,6 +17,16 @@ namespace SocialMVC.Models
         [Display(Name = "Fecha de nacimiento")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime fecha_nacimiento { get; set; }
+        [Display(Name = "Edad")]
+        public int edad { get {
+                DateTime now = DateTime.Today;
+                int edad = now.Year - fecha_nacimiento.Year;
+                if(fecha_nacimiento.Month > now.Month)
+                {
+                    --edad;
+                }
+                return edad;
+            } }
         public string email { get; set; }
         public string contraseña { get; set; }
         [Phone]
